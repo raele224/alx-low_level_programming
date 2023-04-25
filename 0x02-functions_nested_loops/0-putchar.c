7fb1950f8c15d6d54fedf7906f970bb6d740329a
raele224 <rachel.mabuda@gmail.com>
2023-04-25 13:57:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_putchar - prints "_putchar" followed by a new line
+ */
+
+void print_putchar(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+}
