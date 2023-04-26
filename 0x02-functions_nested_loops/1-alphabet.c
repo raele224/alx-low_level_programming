567e8a36765d00119beecd2a844135d6d6b4fe66
raele224 <rachel.mabuda@gmail.com>
2023-04-25 17:06:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return:void 
+*/
+
+void print_alphabet(void)
+{
+char letter = 'a';
+    
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+    
+_putchar('\n');
+}
