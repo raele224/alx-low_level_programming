beb9b6e73f84175b9f3e6c3e0a647cf496da3b43
raele224 <rachel.mabuda@gmail.com>
2023-04-27 01:23:41 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,13 +4,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 int sum = 0;
 
-for (int i = 0; i < 1024; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
+for (int i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
 sum += i;
 }
+
 }
 
 printf("%d\n", sum);
