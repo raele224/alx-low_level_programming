8cbbc0e7e708f8ca7561c30737b01d97f0ff4943
raele224 <rachel.mabuda@gmail.com>
2023-05-02 02:34:34 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: A pointer to a character string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
