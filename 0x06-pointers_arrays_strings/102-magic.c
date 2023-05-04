fffb9a7fe5b497e9a743e4b3b0bde99900abf5dd
raele224 <rachel.mabuda@gmail.com>
2023-05-04 06:06:00 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program sets the value of an integer in an array
+ * without directly referencing the array, only using a pointer.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+*(p + 5) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
