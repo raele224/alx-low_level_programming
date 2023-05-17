743e19f8aeed2268169d7c1e21865a59b97f311b
raele224 <rachel.mabuda@gmail.com>
2023-05-17 02:22:28 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,29 +8,22 @@
  */
 char *_strdup(char *str)
 {
-	if (str == NULL)
-	{
-	return (NULL);
-	}
-
 	unsigned int length = 0;
+	unsigned int i;
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
 
 	while (str[length] != '\0')
-	{
-	length++;
-	}
+		length++;
 
-	char *duplicate = malloc(sizeof(char) * (length + 1));
-
+	duplicate = malloc(sizeof(char) * (length + 1));
 	if (duplicate == NULL)
-	{
-	return (NULL);
-	}
+		return (NULL);
 
-	for (unsigned int i = 0; i <= length; i++)
-	{
-	duplicate[i] = str[i];
-	}
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
 
 	return (duplicate);
 }
