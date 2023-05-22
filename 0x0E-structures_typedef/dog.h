0ac6f7cb461ff3f2c77a0f06042d62099e2afb16
Rachel <rachel.mabuda@gmail.com>
2023-05-22 17:31:00 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ typedef struct dog
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(const dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
 
