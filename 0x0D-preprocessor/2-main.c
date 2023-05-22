32803c5915d7ffbc0b093f481ea04b76e2a0d12d
Rachel <rachel.mabuda@gmail.com>
2023-05-22 18:22:53 +0200

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define FILE_NAME __FILE__
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
