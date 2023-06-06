9a5577bf21dbcb853709388427487474e218ea2b
raele224 <rachel.mabuda@gmail.com>
2023-06-06 14:23:23 +0200

    1-listint_len.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct listint_s
 } listint_t;
 
 /* Function prototype */
+size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 #endif /* LISTS_H */
