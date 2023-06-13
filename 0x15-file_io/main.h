e26e50bca237efb3afc0294e1da63746be8b9335
raele224 <rachel.mabuda@gmail.com>
2023-06-13 02:14:23 +0200

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <unistd.h>
 
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 #endif /* MAIN_H */
